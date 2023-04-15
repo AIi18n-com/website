a5a94721028897dc8cf283a46c30135862a4faa7
Johannlai <johann_lai@outlook.com>
2023-04-15 11:56:42 +0800

    ✨ start a new project

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>AI i18n</span>,
+  project: {
+    link: "https://github.com/AIi18n-com",
+  },
+  docsRepositoryBase: "https://github.com/AIi18n-com",
+  footer: {
+    text: "AIi18n.com",
+  },
+};
+
+export default config;
