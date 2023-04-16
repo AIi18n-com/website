0d5c0e688c01480e41d168f99b4a7d24a6d39a50
Johannlai <johann_lai@outlook.com>
2023-04-16 21:54:12 +0800

    :pencil: Update contents in Feature.md

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,30 +11,7 @@ const config: DocsThemeConfig = {
     // © 2023 AIi18n.com | All rights reserved
     // Made with ❤️ by AIi18n
     // contact us
-    text: (
-      <>
-        © 2023 AIi18n.com | All rights reserved
-        <br />
-        <div>
-          <a
-            href="https://aii18n.com/privacy-policy"
-            style={{ marginLeft: 20 }}
-          >
-            Privacy Policy
-          </a>
-        </div>
-        <div>
-          <a href="mailto: support@aii18n.com" style={{ marginLeft: 20 }}>
-            support@aii18n.com
-          </a>
-        </div>
-        <div>
-          <a href="https://twitter.com/AIi18n" style={{ marginLeft: 20 }}>
-            Twitter
-          </a>
-        </div>
-      </>
-    )
+    text: <>© 2023 AIi18n.com | All rights reserved</>
   }
 }
 
