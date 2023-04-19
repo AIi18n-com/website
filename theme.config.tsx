10378bcbccd585b6e103993894fc7c2e700d16ab
johannli <johannli@tencent.com>
2023-04-19 21:11:37 +0800

    :rocket: Update next.config.js file for improved performance.

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,12 +1,65 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
+const logo = (
+  // left is /aii18n-logo.png
+  // right is AI i18n
+  <div
+    style={{
+      display: 'flex'
+    }}
+  >
+    <img
+      src="/aii18n-logo.png"
+      alt="AI i18n"
+      style={{
+        width: 30,
+        height: 30,
+        marginRight: 10
+      }}
+    />
+    <div
+      style={{
+        fontSize: 24,
+        lineHeight: '30px'
+      }}
+    >
+      AI i18n
+    </div>
+    <style jsx>{`
+      div {
+        mask-image: linear-gradient(
+          60deg,
+          black 25%,
+          rgba(0, 0, 0, 0.2) 50%,
+          black 75%
+        );
+        mask-size: 400%;
+        mask-position: 0%;
+      }
+      div:hover {
+        mask-position: 100%;
+        transition: mask-position 1s ease, -webkit-mask-position 1s ease;
+      }
+    `}</style>
+  </div>
+)
+
 const config: DocsThemeConfig = {
-  logo: <span>AI i18n</span>,
-  project: {
-    link: 'https://github.com/AIi18n-com'
-  },
-  docsRepositoryBase: 'https://github.com/AIi18n-com',
+  logo,
+  head: (
+    <>
+      <meta name="msapplication-TileColor" content="#fff" />
+      <meta name="theme-color" content="#fff" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="AIi18n" />
+      <meta
+        property="og:description"
+        content="AI Solution for Multilingual Website Localization"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  ),
   footer: {
     // © 2023 AIi18n.com | All rights reserved
     // Made with ❤️ by AIi18n
